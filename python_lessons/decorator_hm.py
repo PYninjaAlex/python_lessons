46e64830b04658ccdd6b224873a8459c15487815
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2022-12-26 15:59:48 +0300

    improved dec_hm

--- python_lessons/decorator_hm.py
+++ python_lessons/decorator_hm.py
@@ -1,18 +1,18 @@
 def my_dec(f):
     def wrapper(num=0):
+        output = 'Nothing wrong'
         try:
             res = f(num)
             for j in res:
                 for k in j:
-                    if k.isdigit() == True:
+                    if k.isdigit():
+                        output = 'Error'
                         raise ValueError('Введены неправильные данные. Введите буквы.')
+
         finally:
-            print('Everything good.')
+            print(output)
+        return res
     return wrapper
-
-
-
-
 
 @my_dec
 def func(num):
