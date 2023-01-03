64be5954f9d2dd66017bcca2587a06385f5eab91
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2023-01-03 20:55:15 +0300

    Hm regex 1. Function() update with new data. Folder regex.

--- python_lessons/regex/hm_regex.py
+++ python_lessons/regex/hm_regex.py
@@ -1,8 +1,14 @@
 import re
 
-def date():
-    s = '31.12.2022'
-    result = re.findall(r'[0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]', s)
+date1 = '31.12.2022'
+date2 = '20.08.2014'
+s1 = 'Hello, World!'
+
+def date(str):
+
+    result = re.findall(r'[0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]', str)
     return result
 
-print(date())
+print(date(date1))
+print(date(date2))
+print(date(s1))
