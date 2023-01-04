5886332287c0e52ccdf95a5dd093016d09789794
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2023-01-04 14:40:32 +0300

    Hm regex 2. Function() improved. Folder regex.

--- python_lessons/regex/hm_regex_2.py
+++ python_lessons/regex/hm_regex_2.py
@@ -4,7 +4,7 @@ digit_string = '-900 +5 5 0.8'
 
 def check_digit(str):
 
-    result = re.findall(r'([-+\d])(\.|\d+)|(\d+)', str)
+    result = re.findall(r'([-+\d])(\.\d+|\d+)|(\d+)', str)
     return result
 
 print(check_digit(digit_string))
