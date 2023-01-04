3ec2b45327f84bd767a29ac9552713e4780fe889
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2023-01-04 14:21:46 +0300

    Hm regex 2. Function() updated_2. Folder regex.

--- python_lessons/regex/hm_regex_2.py
+++ python_lessons/regex/hm_regex_2.py
@@ -1,9 +1,10 @@
 import re
 
 digit_string = '-900 +5 5 0.8'
+
 def check_digit(str):
 
-    result = re.findall(r'([-+01-9])(\.|\d+)|(\d+)', str)
+    result = re.findall(r'([-+\d])(\.|\d+)|(\d+)', str)
     return result
 
 print(check_digit(digit_string))
