4f27fdc014c74537e40657767b22b9a30102d64b
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2023-01-04 10:35:57 +0300

    Hm regex 1. Function() modified and new_data added. Folder regex.

--- python_lessons/regex/hm_regex.py
+++ python_lessons/regex/hm_regex.py
@@ -1,16 +1,12 @@
 import re
 
-date1 = '31.12.2022'
-date2 = '20.08.2014'
-s1 = 'Hello, World!'
-s2 = "Today is 03.01.2023, and just recently it was 31.12.2022 last year."
+data = '''Today is 03.01.2023, and just recently it was 31.12.2022 last year.
+    20.08.2014 31.12.2022 45.03.2004 12.15.1998 10.08.1005 00.11.2009
+'''
 
 def date(str):
 
-    result = re.findall(r'[0-9][0-9]\.[0-9][0-9]\.[0-9][0-9][0-9][0-9]', str)
+    result = re.findall(r'(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.([12][09][0-9][0-9])', str)
     return result
 
-print(date(date1))
-print(date(date2))
-print(date(s1))
-print(date(s2))
+print(date(data))
