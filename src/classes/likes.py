d6a5982a356d6e8e8c0608fbefb9d075661ccf0a
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2023-01-19 21:23:19 +0300

    File likes.py. Folder classes in src.

--- src/classes/likes.py
+++ src/classes/likes.py
@@ -1,22 +1,21 @@
 from tkinter import *
 from tkinter import ttk
 
-count = 0
+likes = 0
 def click_button():
-    global count
-    if count == 0:
-        count += 1
+    global likes
+    if likes == 0:
+        likes += 1
     else:
-        count -= 1
-    btn["text"] = f"Likes {count}"
+        likes -= 1
+    btn["text"] = f"Likes {likes}"
 
 
 root = Tk()
 root.title("Likes")
 root.geometry("250x150")
 
-while True:
-    btn = ttk.Button(text="Like", command=click_button)
-    btn.pack()
+btn = ttk.Button(text="Like", command=click_button)
+btn.pack()
 
-    root.mainloop()
+root.mainloop()
